17d693500ff10498fdced7fac57c41cc0fb02227
Brian Forester <brian.forester@proquest.com>
2015-05-10 16:31:22 -0400

    Add test for not enough coins case.

--- test/kata/vending/CoinExchangerTest.java
+++ test/kata/vending/CoinExchangerTest.java
@@ -11,21 +11,19 @@ import java.util.stream.Stream;
 import static org.junit.Assert.*;
 
 public class CoinExchangerTest {
-	private CoinExchanger coinExchanger;
 	private ChangeInCoins coinStatus;
 
-	@Before
-	public void before() {
+	private CoinExchanger buildCoinExchanger(int coinCount) {
 		Map<MeasuredCoin, Integer> inCoinsToCount = new HashMap<>();
-		inCoinsToCount.put(new MeasuredCoin(25), 5);
-		inCoinsToCount.put(new MeasuredCoin(10), 5);
-		inCoinsToCount.put(new MeasuredCoin(5), 5);
-		coinExchanger = new CoinExchanger(inCoinsToCount);
-
+		inCoinsToCount.put(new MeasuredCoin(25), coinCount);
+		inCoinsToCount.put(new MeasuredCoin(10), coinCount);
+		inCoinsToCount.put(new MeasuredCoin(5), coinCount);
+		return new CoinExchanger(inCoinsToCount);
 	}
 
 	@Test
 	public void shouldReturn25() {
+		CoinExchanger coinExchanger = buildCoinExchanger(5);
 		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
 		coinStatus = coinExchanger.getChange(coinsAccumulated, 25);
 		assertEquals(1, coinStatus.getChangeInCoins().getCoinCount());
@@ -34,6 +32,7 @@ public class CoinExchangerTest {
 
 	@Test
 	public void shouldReturn10() {
+		CoinExchanger coinExchanger = buildCoinExchanger(5);
 		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
 		coinStatus = coinExchanger.getChange(coinsAccumulated, 10);
 		assertEquals(1, coinStatus.getChangeInCoins().getCoinCount());
@@ -42,6 +41,7 @@ public class CoinExchangerTest {
 
 	@Test
 	public void shouldReturn5() {
+		CoinExchanger coinExchanger = buildCoinExchanger(5);
 		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
 		coinStatus = coinExchanger.getChange(coinsAccumulated, 5);
 		assertEquals(1, coinStatus.getChangeInCoins().getCoinCount());
@@ -50,6 +50,7 @@ public class CoinExchangerTest {
 
 	@Test
 	public void shouldReturnNickeAndDime() {
+		CoinExchanger coinExchanger = buildCoinExchanger(5);
 		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
 		coinStatus = coinExchanger.getChange(coinsAccumulated, 15);
 		assertEquals(2, coinStatus.getChangeInCoins().getCoinCount());
@@ -62,7 +63,8 @@ public class CoinExchangerTest {
 
 	@Test
 	public void shouldReturnQuartersOneDimeOneNickle() {
-		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
+		CoinExchanger coinExchanger = buildCoinExchanger(5);
+		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();
 		coinStatus = coinExchanger.getChange(coinsAccumulated, 90);
 		assertEquals(5, coinStatus.getChangeInCoins().getCoinCount());
 		assertEquals(90, coinStatus.getChangeInCoins().total());
@@ -72,6 +74,21 @@ public class CoinExchangerTest {
 		assertEquals("Should be 3 quarters", 3, coinCounts.getQuarterCount());
 		assertEquals("Should be 1 dime", 1, coinCounts.getDimeCount());
 		assertEquals("Should be 1 nickle", 1, coinCounts.getNickleCount());
+	}
+
+	@Test
+	public void shouldReturnSuccessFalseAndOriginalCoins() {
+		CoinExchanger coinExchanger = buildCoinExchanger(1);
+		CoinsAccumulated coinsAccumulated = new CoinsAccumulated();;
+		coinsAccumulated = coinsAccumulated.addCoin(new MeasuredCoin(25));
+		coinsAccumulated = coinsAccumulated.addCoin(new MeasuredCoin(25));
+		coinsAccumulated = coinsAccumulated.addCoin(new MeasuredCoin(25));
+		coinsAccumulated = coinsAccumulated.addCoin(new MeasuredCoin(5));
+		coinsAccumulated = coinsAccumulated.addCoin(new MeasuredCoin(10));
+		assertEquals(90, coinsAccumulated.total());
+		coinStatus = coinExchanger.getChange(coinsAccumulated, 90);
+		assertFalse(coinStatus.isSuccessCoinReturn());
+		assertSame(coinsAccumulated, coinStatus.getChangeInCoins());
 	}
 
 
